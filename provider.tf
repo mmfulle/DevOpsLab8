2d3d0d4eb86d17b9c1daafa1493e3f100a7f2548
Mike Fuller <mmfulle@sandia.gov>
2026-06-02 15:21:32 +0000

    iniitial commit

---
+++ provider.tf
@@ -0,0 +1,26 @@
+terraform {
+    backend "azurerm" {
+    resource_group_name = "tfstate"
+    storage_account_name = "storage62329832"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
+    }
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~> 3.0"
+        }
+    }
+
+
+
+}
+provider "azurerm" {
+    features {}
+    client_id = "<Service Principle ID>"
+    client_secret = "<Service Principle Secret>"
+    tenant_id = "<tenant_ID>"
+    subscription_id = "<Subscription_ID>"
+}
+
+
